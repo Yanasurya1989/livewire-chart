484046fe5306d6d364bee10207628238406e985c
YanaWebProgrammer <teukita15@gmail.com>
2024-01-20 09:51:59 +0700

    first commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    @livewireStyles
+    @stack('css')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+    <main class="container py-5">
+        <h1>L</h1>
+        <hr>
+        @yield('content')
+        {{-- @livewire('tutorial') --}}
+    </main>
+
+    @livewireScripts
+    @stack('js')
+</body>
+</html>
